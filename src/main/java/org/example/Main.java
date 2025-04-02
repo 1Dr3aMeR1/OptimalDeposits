e3b27ee03d76ab6b78a11f0ae85141c848254fc3
1Dr3aMeR1 <stepanmalygin0@gmail.com>
2025-04-02 22:18:32 +0300

    x

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,11 @@ import org.example.banks.sberbank.SberbankDeposit;
 
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
         SberbankDeposit deposit = new SberbankDeposit();
         deposit.deposits();
+        System.out.println();
     }
 }
 
